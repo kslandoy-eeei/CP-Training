35ca561f6288c18b4c2803280efbdcf8bd16e33f
kslandoy-eeei <kurt.luis.landoy@eee.upd.edu.ph>
2024-04-20 15:43:58 +0800

    04/20

--- Algo++/UVa 441 - Lotto.cpp
+++ Algo++/UVa 441 - Lotto.cpp
@@ -1,28 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void backtrack(int S, length = 6, string game = "")
-{
-    if (!length)
-    {
-        cout << game;
-    }
-}
-
 int main()
 {
     while (1)
     {
+        bool first = true;
         int k;
-        int nums[6];
-
+        vector<int> nums;
         cin >> k;
+
+        if (k == 0)
+        {
+            break;
+        }
+
+        else if (first)
+        {
+            first = false;
+        }
+
+        else
+        {
+            cout << "\n";
+        }
 
         for (int i = 0; i < k; i++)
         {
-            cin >> nums[i];
+            int n;
+            cin >> n;
+            nums.push_back(n);
         }
 
+        for (int i = 0; i < k - 5; i++)
+        {
+            for (int j = i + 1; j < k - 4; j++)
+            {
+                for (int l = j + 1; l < k - 3; l++)
+                {
+                    for (int m = l + 1; m < k - 2; m++)
+                    {
+                        for (int n = m + 1; n < k - 1; n++)
+                        {
+                            for (int o = n + 1; o < k; o++)
+                            {
+                                printf("%d %d %d %d %d %d", nums[i], nums[j], nums[l], nums[m], nums[n], nums[o]);
+
+                                cout << endl;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
