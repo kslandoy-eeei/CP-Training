4f3035edaa5853e881baba7dbca31ee02d7da5bd
kslandoy-eeei <kurt.luis.landoy@eee.upd.edu.ph>
2024-04-20 22:53:34 +0800

    105

--- Algo++/UVa 105 - The Skyline Problem.cpp
+++ Algo++/UVa 105 - The Skyline Problem.cpp
@@ -1,24 +1,11 @@
 /*
+Create a vector of heights (named heights) with initial values set to zero.
+For each input Li, Hi, Ri, modify the vector such that:
+for all i in range [Li, Ri], 
+heights[i] = max(heights[i], Hi)
 
-This must be a correct solution.
-Say, we have a vector of heights {h1, h2, h3, h4, ..., hn};
-where hi is the maximmum height of the buildings at coordinate i.
-
-This solution goes like this:
-After coming up with the vector of heights, iterate through the vector and if there is a height change at index i,
-print i and the height at i.
-
-The testcase given on UVa has the following final heights vector:
-11 11 13 13 13 13 13 13 13 0 0 7 7 7 7 7 3 3 18 18 18 18 13 13 13 13 13 13 13
-
-The output is:
-1 11 3 13 9 0 12 7 16 3 19 18 22 3 23 13 29 
-
-The first three numbers (1, 11, 3) show that everytime the height changes at index i, i is printed along with the ith height.
-In the next case (3, 13, 9) however, the height changed at index 10, but 9 was printed along with the 10th height.
-
-Test case is flawed I suppose.
-
+After taking all inputs, iterate through the heights vector.
+For each change in height value, print the index and the height.
 */
 
 #include <bits/stdc++.h>
@@ -45,7 +32,7 @@ int main()
             rightmost = r;
         }
 
-        for (int i = l; i < r + 1; i++)
+        for (int i = l; i < r; i++)
         {
             heights[i] = max(heights[i], h);
         }
@@ -61,20 +48,17 @@ int main()
 
     int currentHeight;
 
-    for (int i = leftmost; i < rightmost + 1; i++)
+    for (int i = leftmost; i < rightmost + 1; ++i)
     {
         if (i == leftmost)
         {
             printf("%d %d ", i, heights[i]);
             currentHeight = heights[i];
         }
-
         else if (currentHeight != heights[i])
         {
             printf("%d %d ", i, heights[i]);
             currentHeight = heights[i];
         }
     }
-
-    cout << rightmost << " 0";
 }
