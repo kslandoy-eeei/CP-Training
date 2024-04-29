deda69311e4b4cc810764f6c2663968e8d47f9cf
kslandoy-eeei <kurt.luis.landoy@eee.upd.edu.ph>
2024-04-29 12:04:06 +0800

    10038 AC

--- UHunt/UVa 10038.cpp
+++ UHunt/UVa 10038.cpp
@@ -7,31 +7,30 @@ int main()
 
     while (cin >> n)
     {
-        vector<int> nums(n);
+        vector<int> nums(n), diffs(n);
+        bool jolly = true;
         for (int i = 0; i < n; i++)
         {
             cin >> nums[i];
+
+            if (i != 0)
+            {
+                diffs[i] = abs(nums[i] - nums[i-1]);
+            }
         }
 
-        bool jolly = true;
+        sort(diffs.begin(), diffs.end());
 
         for (int i = 0; i < n - 1; i++)
         {
-            if (abs(nums[i] - nums[i + 1]) >= n || abs(nums[i] - nums[i + 1]) == 0)
+            if (diffs[i] == diffs[i + 1] || diffs[i] >= n || diffs[i + 1] >= n)
             {
                 jolly = false;
                 break;
             }
         }
 
-        if (jolly)
-        {
-            cout << "Jolly\n";
-        }
-
-        else
-        {
-            cout << "Not jolly\n";
-        }
+        if (jolly) cout << "Jolly\n";
+        else cout << "Not jolly\n";
     }
 }
